5147b07ababfd02a470a41f4002c5c8157069e46
CCCCoder <15505514373@163.com>
2019-04-15 23:31:13 +0800

    feat: 新增启动页。

---
+++ app/src/main/java/com/n1njac/cmovie/startup/LauncherActivity.kt
@@ -0,0 +1,48 @@
+package com.n1njac.cmovie.startup
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.n1njac.cmovie.MainActivity
+import com.n1njac.cmovie.R
+import kotlinx.android.synthetic.main.activity_launcher.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
+
+/**
+ * Created by N1njaC on 2019/4/15 22:28.
+ * Copyright (c) 2019 IFLYTEK CO.,LTD. All rights reserved.
+ * Mail:aiai173cc@gmail.com
+ */
+class LauncherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launcher)
+        setTimeFormat()
+        delayStartUp()
+    }
+
+    private fun setTimeFormat() {
+        val format = SimpleDateFormat("yyyy年MM月dd日,EEEE", Locale.getDefault())
+        time_tv.text = format.format(System.currentTimeMillis())
+    }
+
+    private fun delayStartUp() {
+        GlobalScope.launch {
+            delay(3000)
+            startActivity(Intent(this@LauncherActivity, MainActivity::class.java))
+            finish()
+        }
+
+//        Handler().postDelayed({
+//            startActivity(Intent(this@LauncherActivity, MainActivity::class.java))
+//            finish()
+//        }, 5000L)
+    }
+}
