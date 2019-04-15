b5dfeacd08d771fbd01ec47670c58dbc4f3d9a36
CCCCoder <15505514373@163.com>
2019-04-15 22:21:58 +0800

    feat: 添加必要依赖.

--- app/src/main/java/com/n1njac/cmovie/MainActivity.kt
+++ app/src/main/java/com/n1njac/cmovie/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.n1njac.cmovie
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
---
+++ app/src/main/java/com/n1njac/cmovie/app/CMApplication.kt
@@ -0,0 +1,14 @@
+package com.n1njac.cmovie.app
+
+import android.app.Application
+
+/**
+ * Created by N1njaC on 2019/4/15 22:11.
+ * Copyright (c) 2019 IFLYTEK CO.,LTD. All rights reserved.
+ * Mail:aiai173cc@gmail.com
+ */
+class CMApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
