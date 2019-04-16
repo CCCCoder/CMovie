0ccab44bb5a4300f472ecc6b19be588a2c1fa7c2
CCCCoder <15505514373@163.com>
2019-04-16 22:57:13 +0800

    feat: 确定API为时光网。

---
+++ app/src/main/java/com/n1njac/cmovie/net/URLs.kt
@@ -0,0 +1,10 @@
+package com.n1njac.cmovie.net
+
+/**
+ * Created by N1njaC on 2019/4/16 22:14.
+ * Copyright (c) 2019 IFLYTEK CO.,LTD. All rights reserved.
+ * Mail:aiai173cc@gmail.com
+ */
+
+const val BASE_API_MTIME = "https://api-m.mtime.cn/"
+const val URL_TICKETING = "PageSubArea/HotPlayMovies.api"
